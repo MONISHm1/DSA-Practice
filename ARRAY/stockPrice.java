59abed817d7d3e3896708c05e4e10167bf9b57c4
MONISHM1 <monishshah795@gmail.com>
2026-03-25 17:53:35 +0530

    stock and trapping rain water prob

---
+++ ARRAY/stockPrice.java
@@ -0,0 +1,20 @@
+public class stockPrice {
+    public static int priceStok(int prices[] ){
+        int buyingPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0; i< prices.length; i++){
+            if(buyingPrice < prices[i]){
+                int profit = prices[i] - buyingPrice;
+                maxProfit = Math.max(profit, maxProfit);
+            }
+           else{
+             buyingPrice = prices[i];
+           }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices [] = {7,1,5,3,6,4};
+        System.out.println(priceStok(prices));
+    }
+}
---
+++ ARRAY/trappingRainWater.java
@@ -0,0 +1,25 @@
+public class trappingRainWater {
+    public static int trapping(int height[]){
+        int h = height.length;
+        int leftMax[] = new int[h];
+        leftMax[0] = height[0];
+        for(int i=1; i<h; i++){
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+
+        int rightMax[] = new int[h];
+        rightMax[h-1] = height[h-1];
+        for(int i = h-2; i>=0; i--){
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+        int trappedWater = 0;
+        for(int i = 0; i<h; i++){
+        trappedWater += (Math.min(leftMax[i], rightMax[i])) - height[i]; 
+        }
+        return trappedWater;
+    }
+    public static void main(String[] args) {
+        int height[] = {4,2,0,6,3,2,5};
+        System.out.println(trapping(height));
+}
+}
