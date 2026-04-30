daed2438feee846a332983183e71538b2fb0877f
MONISHM1 <monishshah795@gmail.com>
2026-04-30 10:24:49 +0530

    queue is just started

---
+++ QUEUE/circularQueue.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class circularQueue {
+    static class circular {
+        int arr[];
+        int size;
+        int front;
+        int rear;
+
+        circular(int n) {
+            arr = new int[n];
+            size = n;
+            front = -1;
+            rear = -1;
+        }
+
+        public boolean isEmpty() {
+            return rear == -1 && front == -1;
+        }
+
+        public boolean isFull() {
+            return (rear + 1) % size == front;
+        }
+
+        public void add(int data) {
+            if (isFull()) {
+                System.out.println("queue is full");
+                return;
+            }
+            if (isEmpty()) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
+            arr[rear] = data;
+        }
+
+        public int remove() {
+            if (isEmpty()) {
+                System.out.println("queue is empty");
+                return -1;
+            }
+            int result = arr[front];
+
+            if (rear == front) {
+                rear = front = -1;
+            } else {
+                front = (front + 1) % size;
+            }
+            return result;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("queue is empty");
+                return -1;
+            }
+            return arr[front];
+        }
+    }
+
+    public static void main(String[] args) {
+        circular q = new circular(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
